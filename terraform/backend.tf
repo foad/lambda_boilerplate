76f080a1ea26c36b327df139b98e54aa84a1e72f
Dan Foad <dan@foad.dev>
2025-08-12 18:09:37 +0100

    remove unnecessary comments and terraform setup

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,22 +1,5 @@
-# Terraform Backend Configuration
-# This file configures remote state storage in S3 with DynamoDB locking
-
 terraform {
-  backend "s3" {
-    # These values will be provided via terraform init -backend-config
-    # bucket         = "terraform-state-serverless-todo-api"
-    # key            = "environments/{environment}/terraform.tfstate"
-    # region         = "eu-west-2"
-    # dynamodb_table = "{environment}-terraform-state-lock"
-    # encrypt        = true
-  }
+  backend "s3" {}
 }
 
-# Random suffix for unique bucket naming
-resource "random_id" "bucket_suffix" {
-  byte_length = 4
-}
 
-# Note: Remote state is now managed externally via setup script
-# S3 bucket and configuration are created by scripts/setup-remote-state.sh
-# No Terraform resources needed here since we use native S3 locking
